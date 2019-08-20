5e41f5e0743e29a33354f67104cfd2a6f4884ea4
chenyinhai <cyhaie@163.com>
2019-08-20 09:50:52 +0800

    初始化仓库

---
+++ checkphoneNum/NSString+checkNum.h
@@ -0,0 +1,19 @@
+//
+//  NSString+checkNum.h
+//  wisdomSite
+//
+//  Created by Macx on 16/4/14.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (checkNum)
+
+
+/**
+ 检测是否是手机号，不能保证最新号码能验证
+ */
+-(BOOL)checkPhoneNumInput;
+
+@end
